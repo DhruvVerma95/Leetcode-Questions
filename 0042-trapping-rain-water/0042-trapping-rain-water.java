2df7162009faceb99b685acd957f679ac3c76cf3
Dhruv Verma <92014417+DhruvVerma95@users.noreply.github.com>
2025-02-02 15:18:45 +0530

    Time: 0 ms (100%), Space: 47.1 MB (8.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,29 @@
 class Solution {
     public int trap(int[] height) {
         if (height == null || height.length == 0) return 0;
-
-        int n = height.length;
-        int[] maxL = new int[n];
-        int[] maxR = new int[n];
+        
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
         int trappedWater = 0;
-
-        // Compute maxL (maximum height to the left of each index)
-        maxL[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            maxL[i] = Math.max(maxL[i - 1], height[i]);
+        
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    trappedWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    trappedWater += rightMax - height[right];
+                }
+                right--;
+            }
         }
-
-        // Compute maxR (maximum height to the right of each index)
-        maxR[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            maxR[i] = Math.max(maxR[i + 1], height[i]);
-        }
-
-        // Compute trapped water at each index
-        for (int i = 0; i < n; i++) {
-            trappedWater += Math.min(maxL[i], maxR[i]) - height[i];
-        }
-
+        
         return trappedWater;
     }
 }
